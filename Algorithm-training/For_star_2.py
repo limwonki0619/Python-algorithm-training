4e2589e37dc004b71e2facfb8f4c12b62b81c535
Wonki Lim <51067315+limwonki0619@users.noreply.github.com>
2019-07-21 11:23:38 +0900

    Update For_star_2.py

--- Algorithm-training/For_star_2.py
+++ Algorithm-training/For_star_2.py
@@ -1,12 +1,13 @@
 Height = 41
 
 def star():
-    Height = int(input('Height 값은 33만 가능합니다.'))
-    denominator = 4
-
+    Height = int(input('Height 값은 현재 '33'만 가능합니다.'))
+    denominator_1 = 4
+    denominator_2 = 5
+    
     print(' '*19, '*')  # Part 1
     
-    for P_2 in range(Height // denominator):    # Part 2, i = 0~8
+    for P_2 in range(Height // denominator_1):  # Part 2, i = 0 ~ 8
         for P_2_blank_1 in range(P_2, 17-P_2):  # i:17-i
             print(' ', end='')
         for P_2_star_1 in range(P_2, P_2+1):    #  i:i+1
@@ -21,7 +22,7 @@ def star():
             print('*', end='')
         print()
 
-    for P_3 in range(Height // denominator):  # Part_3
+    for P_3 in range(Height // denominator_2):  # Part_3, i = 0 ~ 6
         for P_3_blank_1 in range(P_3, P_3+1):
             print(' ', end='')
         for P_3_star_1 in range(P_3, P_3+1):
@@ -36,7 +37,7 @@ def star():
             print('*', end='')
         print()
 
-    for P_4 in reversed(range(Height // denominator)):  # Part_4
+    for P_4 in reversed(range(Height // denominator_1)):  # Part_4
         for P_4_blank_1 in range(P_4, P_4 + 1):
             print(' ', end='')
         for P_4_star_1 in range(P_4, P_4 + 1):
@@ -51,7 +52,7 @@ def star():
             print('*', end='')
         print()
 
-    for P_5 in reversed(range(Height // denominator)):    # Part_5
+    for P_5 in reversed(range(Height // denominator_2)):    # Part_5
         for P_5_blank_1 in range(P_5, 17-P_5):
             print(' ', end='')
         for P_5_star_1 in range(P_5, P_5+1):
